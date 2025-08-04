aabb44fe80ab5348ba468e1be467dbaa743ce141
azzi0130-ux <azzi0130@gmail.com>
2025-08-04 11:49:15 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.title('AI 세계 문화 탐험 일기')
 
 # 여기에 당신의 '새롭고 유효한' 실제 API 키를 큰따옴표 안에 넣으세요!!!
 # 이것이 우리의 마지막 희망입니다.
-my_api_key = "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"
+my_api_key = "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"
 
 country = st.selectbox('나라를 선택하세요', ['이집트', '페루', '브라질', '몽골'])
 environment = st.selectbox('자연환경을 선택하세요', ['사막', '고산지대', '열대 우림', '초원'])
